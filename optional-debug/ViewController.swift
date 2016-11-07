d8ada18cd96cdb7620b33b2c4c8e1b9c5e64796c
khacviet <khacviet@techmasters-mac-mini.local>
2016-11-07 09:53:00 +0700

    Initial Commit

---
+++ optional-debug/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  optional-debug
+//
+//  Created by khacviet on 11/7/16.
+//  Copyright © 2016 khacviet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
